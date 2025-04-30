9959af83641dc33d7f1ac49f0bca8ef264ee180c
Michael Creel <151579224+MichaelCreel@users.noreply.github.com>
2025-04-30 09:31:40 -0600

    Comment Cleanup & Comment Hints
    Cleaned up comments that were just unused code. Added comments with hints on what to pass into deform methods in case variable names are not clear. Deformation still has rotation and scale based issues with deforming models.

--- Deformer.cs
+++ Deformer.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class Deformer : MonoBehaviour
 {
-    //Deform only works when the call is from the deforming object's collision
+    //Deform requires the collision from the deforming object
+    //Deforming Collider, Collision, Deforming Mesh, Contact Points, Resistance to Deforming, Maximum Distance for Full Deforming, Radius for Functional Deforming, Deforming Multiplier, Minimum Distance for Deforming, Radius for Outward Deforming (Push Out & Up)
     public void Deform(MeshCollider collider, Collision collision, Mesh mesh, ContactPoint[] contacts, float deformResistance, float buffer, float radius, int multiplier, float bufferCut, float outformRadius)
     {
         Vector3 rotation = collider.gameObject.transform.rotation.eulerAngles;
@@ -17,27 +19,18 @@ public class Deformer : MonoBehaviour
 
         foreach (var vertex in vertices)
         {
-            Vector3 positionBase = new Vector3(vertex.x * scale.x * rotation.z/360f, vertex.y * scale.y * rotation.x/360f, vertex.z * scale.z * rotation.y/360f);
-            Debug.DrawLine(positionBase + collider.gameObject.transform.position, positionBase + new Vector3(0, 0, 0.05f) + collider.gameObject.transform.position, Color.red, 30f);
+            Vector3 positionBase = new Vector3(vertex.x * scale.x, vertex.y * scale.y, vertex.z * scale.z);
+            Debug.DrawLine(positionBase + collider.gameObject.transform.position, positionBase + new Vector3(0, 0, 0.2f) + collider.gameObject.transform.position, Color.red, 30f);
         }
-        Debug.Break();
-
-        //Vector3 rotation = collider.gameObject.transform.rotation.eulerAngles;
-
-        Debug.Log(rotation);    
 
         foreach (ContactPoint contact in contacts)
         {
-            //Vector3 closP = (collider.ClosestPoint(contact.point) - collider.gameObject.transform.position);
             Vector3 closP = ClosestPoint(contact.point, vertices);
-
-            //Debug.Log(collider.gameObject.transform.rotation);
 
             Vector3 vertexPos = new Vector3(closP.x / scale.x, closP.y / scale.y, closP.z / scale.z);
 
             Vector3 closestPoint = vertexPos;
             Vector3 change = contact.impulse / Time.fixedDeltaTime / deformResistance / -10000 * (float)multiplier;
-            //vertexPos += new Vector3(change.x / scale.x * rotation.x / 360f, change.y / scale.y * rotation.y / 360f, change.z / scale.z * rotation.z / 360f);
             vertexPos += new Vector3(change.x / scale.x, change.y / scale.y, change.z / scale.z);
 
             for (int i = 0; i < vertices.Length; i++)
@@ -66,12 +59,13 @@ public class Deformer : MonoBehaviour
         collider.sharedMesh = mesh;
     }
 
+    //DeformVertex requires the collision from the deforming object
+    //Deforming Collider, Collision, Deforming Mesh, Contact Points, Resistance to Deforming, Maximum Distance for Full Deforming, Radius for Functional Deforming, Deforming Multiplier, Minimum Distance for Deforming, Radius for Outward Deforming(Push Out & Up)
     public void DeformVertex(MeshCollider collider, Collision collision, Mesh mesh, ContactPoint contact, float deformResistance, float buffer, float radius, int multiplier, float bufferCut, float outformRadius)
     {
         Vector3[] vertices = new Vector3[mesh.vertices.Length];
         vertices = mesh.vertices;
 
-        //Vector3 closP = collider.ClosestPoint(contact.point) - collider.gameObject.transform.position;
         Vector3 closP = ClosestPoint(contact.point, vertices);
 
         Vector3 scale = collider.gameObject.transform.localScale;
@@ -131,6 +125,7 @@ public class Deformer : MonoBehaviour
                 point = vertices[i];
             }
         }
+        Debug.Log(point);
         return point;
     }
 }
